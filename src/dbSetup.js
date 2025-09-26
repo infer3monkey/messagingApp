f9fe49fa99286c5ab6164943ebb672e95a00be5d
Daniel Ordaz <fer.dani.ordaz@gmail.com>
2025-09-25 20:55:01 -0700

    created friends and channels database. added channel_id to messages table. 1 is for global chat

--- src/dbSetup.js
+++ src/dbSetup.js
@@ -11,16 +11,48 @@ async function setupTables() {
       );
     `);
 
+    // Create the channels table
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS channels (
+        id SERIAL PRIMARY KEY
+      );
+    `);
+
     // Create the messages table which stores all messages from global chat
     // will likely change format as message channels get added
     await pool.query(`
       CREATE TABLE IF NOT EXISTS messages (
         id SERIAL PRIMARY KEY,
+        channel_id INTEGER REFERENCES channels(id) DEFAULT 1,
         user_id INTEGER REFERENCES users(id),
         text TEXT,
         edited BOOLEAN DEFAULT FALSE,
         timestamp TEXT
       );
+    `);
+
+    // Create the friends table
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS friends (
+        id SERIAL PRIMARY KEY,
+        user_id INTEGER REFERENCES users(id),
+        friend_user_id INTEGER REFERENCES users(id),
+        channel_id INTEGER REFERENCES channels(id),
+        accepted BOOLEAN DEFAULT FALSE
+      );
+    `);
+
+    // Creating indexes on foreign keys for efficiency
+    await pool.query('CREATE INDEX IF NOT EXISTS idx_messages_channel_id ON messages(channel_id)');
+    await pool.query('CREATE INDEX IF NOT EXISTS idx_friends_user_id ON friends(user_id)');
+    await pool.query('CREATE INDEX IF NOT EXISTS idx_friends_friend_user_id ON friends(friend_user_id)');
+    await pool.query('CREATE INDEX IF NOT EXISTS idx_friends_channel_id ON friends(channel_id)');
+
+    // Inserting Global Chat on Creation, if it doesn't exist at id 1
+    await pool.query(`
+      INSERT INTO channels (id)
+      SELECT 1
+      WHERE NOT EXISTS (SELECT 1 FROM channels WHERE id = 1);
     `);
 
     console.log('Tables created!');
