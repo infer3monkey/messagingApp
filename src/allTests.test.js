32da2eb04cb6db9f9bd27dcbedb0b955f6d7204d
Daniel Ordaz <fer.dani.ordaz@gmail.com>
2025-10-15 14:16:21 -0700

    finished all test cases for checkIfValidToken, all test cases are passing

--- src/allTests.test.js
+++ src/allTests.test.js
@@ -1,8 +1,7 @@
-// Importing Functions for Testing
+// Importing Functions for Testing, Using Jest for Testing
 import {checkIfValidToken} from '../public/js/utils.js'
-// Allowing Jest to Simulate Proper Fetches Through Node
 import { jest, test, expect } from '@jest/globals';
-//import fetch from 'node-fetch'
+
 global.fetch = jest.fn();
 
 // Tests for checkIfValidToken
@@ -12,15 +11,11 @@ describe('checkIfValidToken', () => {
         fetch.mockResolvedValueOnce({ status: 200 }); // Simulating Invalid Response From Server
         // Allowing function to change window location without errors
         Object.defineProperty(global, 'window', {
-            value: {
-            location: {
-                href: '',
-            },
-            },
+            value: { location: { href: '' } },
             writable: true,
         });
         // Passing in a valid Token to the function, will fail if database is reset
-        const result = await checkIfValidToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzYwNTUyMjc2LCJleHAiOjE3NjA2Mzg2NzZ9.JP8lZWokjJEqMwXCnM8DNCyQxpbRePxfJjNcQHiGs30');
+        const result = await checkIfValidToken('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNzYwNTYxODE4LCJleHAiOjE3NjA2NDgyMTh9.TyglCpVwytPrmNGHvI9oOQoVdqMOLDPwi9JN4GugY2A');
         expect(result).toBe('Valid Token');
     })
 
@@ -28,17 +23,21 @@ describe('checkIfValidToken', () => {
         fetch.mockResolvedValueOnce({ status: 401 }); // Simulating Invalid Response From Server
         // Allowing function to change window location without errors
         Object.defineProperty(global, 'window', {
-            value: {
-            location: {
-                href: '',
-            },
-            },
+            value: { location: { href: '' } },
             writable: true,
         });
         const result = await checkIfValidToken('token123'); // Passing in an invalid Token to the function
         expect(result).toBe('Invalid Token');
     })
 
-
+    test('Server Error Returning Error Validating Token', async () => {
+        fetch.mockRejectedValueOnce(new Error('Network error')); // Simulating Server Failure, notice rejected instead of resolved
+        // Allowing function to change window location without errors
+        Object.defineProperty(global, 'window', {
+            value: { location: { href: '' } },
+            writable: true,
+        });
+        const result = await checkIfValidToken(''); // Token Passed In Does not Matter
+        expect(result).toBe('Error Validating Token');
+    })
 })
-
